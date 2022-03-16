3c63811e5c5455c99bff9477d83864b2231ef09c
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-03-16 11:56:42 +0100

    probamos nueva libreria y su modo de importarlo

--- src/app/clases/enlace.ts
+++ src/app/clases/enlace.ts
@@ -1,10 +1,10 @@
 import { v4 } from 'uuid';
 import { random as _ru } from 'underscore';
-import {enc} from 'crypto-js';
+const Cryptr = require('cryptr');
+
+const criptoestancia = new Cryptr('9e3427cee6e1441645c2b307b31517524fc2a1b2b1e073649e');
 
 export class Enlace {
-    
-    private key:string = '9e3427cee6e1441645c2b307b31517524fc2a1b2b1e073649e';
     
     private matchid:string;
     private matchpass:string;
@@ -12,7 +12,7 @@ export class Enlace {
     
     constructor(code?:string){
         if(code){
-            const { matchid , matchpass , player } = JSON.parse(AES.decrypt(code,this.key).toString(enc.Utf8));
+            const { matchid , matchpass , player } = criptoestancia.decrypt(code);
             this.matchid = matchid ; this.matchpass = matchpass ; this.jugador = player;
         }else{
             this.matchid = v4();
@@ -23,7 +23,7 @@ export class Enlace {
 
     public get link(){
         let data = { matchid:this.matchid , matchpass:this.matchpass , player:this.jugador }
-        return `http://localhost:4200/${AES.encrypt(JSON.stringify(data),this.key).toString()}`;
+        return `http://192.168.0.14:4200/${criptoestancia.encrypt(JSON.stringify(data))}`;
     }
 
     public get info(){
